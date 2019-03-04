594c7616ac6b2dd5e989e3feaa54520e6db06833
Mário Antunes <mario.antunes@gmail.com>
2019-03-04 09:55:53 +0000

    Fix int convertion.

--- utils.py
+++ utils.py
@@ -3,13 +3,16 @@ import argparse
 
 
 def check_positive_number(number):
-    return number > 0
+    ivalue = int(number)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % number)
+    return ivalue
 
 
 def check_port(port, base=1024):
     ivalue = int(port)
     if ivalue <= base:
-        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+        raise argparse.ArgumentTypeError("%s is an invalid port value" % port)
     return ivalue
 
 
