ef8a48c620cf2ee1e6e7b2615c67cfc9562659c9
Diogo Gomes <dgomes@av.it.pt>
2019-03-09 21:45:06 +0000

    simpler

--- utils.py
+++ utils.py
@@ -1,20 +1,10 @@
 import time
 import argparse
 
-
-def check_positive_number(number):
-    ivalue = int(number)
-    if ivalue <= 0:
-        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % number)
-    return ivalue
-
-
-def check_port(port, base=1024):
-    ivalue = int(port)
-    if ivalue <= base:
-        raise argparse.ArgumentTypeError("%s is an invalid port value" % port)
-    return ivalue
-
+ORDER = 0
+REQ_TASK = 1
+TASK_READY = 2
+PICKUP = 3
 
 def work(seconds):
     time.sleep(seconds)
